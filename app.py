9727d6beb5b3b6fe743c567ee9b30758d8cf0e3b
Shruti Pakhare <shrupakhare@gmail.com>
2022-10-22 02:01:01 +0530

    Docker and Github action change is commit

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data =[float(x) for x in request.form.values()]
